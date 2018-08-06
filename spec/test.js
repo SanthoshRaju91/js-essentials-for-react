c32e13f238f76a68ae73bd0db596e5290ab888f0
santhoshRaju91 <santhosh.codeholic@gmail.com>
2018-08-06 10:41:03 +0530

    Commenting for documentation

--- spec/test.js
+++ spec/test.js
@@ -1,1 +1,10 @@
+/**
+ * This test is picked up by mocha to run the tests.
+ * So the file or the module you imported will be executed
+ *
+ * Eg:
+ * "../modules/arrow/exercise" - exercise
+ * "../modules/arrow/solution" - executing the solution
+ */
+
 import "../modules/block-scopes/solution";
